be6c1c8174ed00ba6417d8b30a6b94ea15ab5858
Jody Linder <linder.jody@gmail.com>
2018-03-02 10:45:47 +1100

    Working javascript for search

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,17 @@
- 
 function myFunction() {
+    // Declare variables
     var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
+    input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById("gallery");
-    li = ul.getElementsByTagName("li");
+    li = ul.getElementsByTagName('li');
+    // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1 || $(a).data("title") === filter.toLocaleLowerCase()) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
-
         }
     }
 }
- 
-
